ddaafb76e0ea57cf1c49028060b605d6a9f17716
Oudani Achraf <achraf.oudani@gmail.com>
2021-03-12 23:42:07 +0100

    git init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+Use React and the data below to display a list of users alongside their favorite movies.
+
+For detailed instructions, refer to instructions.md.
+*/
+
+const profiles = [
+  {
+    id: 1,
+    userID: "1",
+    favoriteMovieID: "1",
+  },
+  {
+    id: 2,
+    userID: "2",
+    favoriteMovieID: "1",
+  },
+  {
+    id: 3,
+    userID: "4",
+    favoriteMovieID: "5",
+  },
+  {
+    id: 4,
+    userID: "5",
+    favoriteMovieID: "2",
+  },
+  {
+    id: 5,
+    userID: "3",
+    favoriteMovieID: "5",
+  },
+  {
+    id: 6,
+    userID: "6",
+    favoriteMovieID: "4",
+  },
+];
+
+const users = {
+  1: {
+    id: 1,
+    name: "Jane Cruz",
+    userName: "coder",
+  },
+  2: {
+    id: 2,
+    name: "Matthew Johnson",
+    userName: "mpage",
+  },
+  3: {
+    id: 3,
+    name: "Autumn Green",
+    userName: "user123",
+  },
+  4: {
+    id: 4,
+    name: "John Doe",
+    userName: "user123",
+  },
+  5: {
+    id: 5,
+    name: "Lauren Carlson",
+    userName: "user123",
+  },
+  6: {
+    id: 6,
+    name: "Nicholas Lain",
+    userName: "user123",
+  },
+};
+
+const movies = {
+  1: {
+    id: 1,
+    name: "Planet Earth 1",
+  },
+  2: {
+    id: 2,
+    name: "Selma",
+  },
+  3: {
+    id: 3,
+    name: "Million Dollar Baby",
+  },
+  4: {
+    id: 4,
+    name: "Forrest Gump",
+  },
+  5: {
+    id: 5,
+    name: "Get Out",
+  },
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <h2>My Favorite Movie</h2>
+          {profiles.map((profile) => {
+            const user = users[profile.userID].name;
+            const favoriteMovieName = movies[profile.favoriteMovieID].name;
+            return (
+              <li key={profile.userID}>
+                {`${user} favorite movie is ${favoriteMovieName}`}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
